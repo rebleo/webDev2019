4f131426a7d45bbe313643339ae20e926722ea8d
rebleo <rml444@nyu.edu>
2019-10-31 15:12:30 -0400

    week09 - js

--- week09/code/potato.js
+++ week09/code/potato.js
@@ -1,22 +1,67 @@
 console.log("yay!");
-let theBody = document.querySelector("body");
-// console.log(theBody.style);
-// let's make the button do something!
-const theButton = document.querySelector("#button1");
-// console.log(theButton);
-theButton.addEventListener('click', clickButton);
 //
-//
-const theOtherButton = document.querySelector("#button2");
-// when theOtherButton is clicked, trigger the changeColor function
-theOtherButton.addEventListener('click', changeColor);
-
-function changeColor() {
-  console.log("change color");
-  theBody.style.color = "red";
-  theBody.style.backgroundColor = "orange";
+// string data
+let myFirst = 'rebecca';
+let myLast = 'leopold';
+console.log(myFirst + " " + myLast);
+// numeric data
+let theQuanity = 19;
+let thePrice = .50;
+// let theTotal = theQuanity * thePrice;
+function theCalculator(theQuantity, thePrice) {
+  return theQuanity * thePrice;
 }
-
+//
+// arrays!
+let theEmpty = [];
+var theYear = ["January", "Feb", "March", "April", "May", "June"];
+console.log(theYear.length);
+for (var i = 0; i < theYear.length; i++) {
+  console.log(theYear[i])
+}
+// splice removes items + push adds
+// theYear.splice(0, theYear.length);
+theYear.push("July");
+console.log(theYear)
+//
+// put the html element into a variable
+const myButton = document.querySelector("#button1")
+// add an eventListener, a js event + yr call back
+myButton.addEventListener('click', clickButton)
+// this is callback function
 function clickButton() {
   console.log("clicked!");
 }
+//
+// put the html body into a variable
+const myBody = document.querySelector('body');
+// check out the style properties of that element!
+console.log(myBody.style)
+const myOtherButton = document.querySelector("#button2")
+myOtherButton.addEventListener('click', changeColors)
+// call backfunction to respond when users clicks
+function changeColors() {
+  console.log("colors changed");
+  // change the font color
+  myBody.style.color = "yellow";
+  // change the background color
+  myBody.style.backgroundColor = "rebeccapurple"
+}
+//
+//
+var theText = document.querySelector('h3');
+// this is ES6 arrow notation
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+document.addEventListener('keydown', theEvent => {
+  if (theEvent.keyCode === 32) {
+    console.log("key 32!");
+    theText.textContent = "you pressed space!"
+  } else {
+    if (theEvent.keyCode === 82) {
+      console.log("key 82!");
+      theText.textContent = "you pressed r!"
+    }
+  }
+})
+//
+///
--- week10/week10.md
+++ week10/week10.md
@@ -1,16 +1,26 @@
+##### Longer references:
+* [MDN JS Tutorial](https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript)
+* [Eloquent JavaScript](http://eloquentjavascript.net/) - Marijn Haverbeke
+* [Learning PHP, MySQL & JavaScript](http://lpmj.net/5thedition/) - Robin Nixon
 
+##### Links:
 * [JS Event Reference](https://developer.mozilla.org/en-US/docs/Web/Events)
 * [Query Selector](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector)
 * [querySelectorAll](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorall)
 * [addEventListener](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
 * [removeEventListener](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener)
 * [classList](https://developer.mozilla.org/en-US/docs/Web/API/Element/classList)
+* [ES 6 Arrow Functions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions)
 
 
-*** 
+***
 * [JIT Compilation](https://hacks.mozilla.org/2017/02/a-crash-course-in-just-in-time-jit-compilers/)
 * [Hoisting](https://developer.mozilla.org/en-US/docs/Glossary/Hoisting)
 * [Dynamically Typed (Variables)](https://stackoverflow.com/questions/1517582/what-is-the-difference-between-statically-typed-and-dynamically-typed-languages)
 * [Truthy](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) + [Falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy)
 
 * [String Styling](https://google.github.io/styleguide/jsguide.html#features-strings-use-single-quotes)
+
+***
+[Chrome Dev Tools Console Ref]
+(https://developers.google.com/web/tools/chrome-devtools/console/reference)
